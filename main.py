e0e454abdf526afb8e0af37842dd2e8aa48ddb78
Rotten-Manuel <rottenimugi@gmail.com>
2025-11-29 18:51:14 -0500

    some try to this ai API

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("not prompt provided (usage: main.py \"promt\")")
+        sys.exit(1)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=f"{sys.argv[1]}"
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
