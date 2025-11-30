5f7e93193c7f30f2d2f046a348a5532f5495855a
Rotten-Manuel <rottenimugi@gmail.com>
2025-11-29 19:14:02 -0500

    now it should work

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=f"{sys.argv[1]}"
+        contents=f"{sys.argv[1:]}"
     )
 
     print(response.text)
